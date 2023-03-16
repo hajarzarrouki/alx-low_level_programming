804a481ffb2eea0ad478b5dad58cee3d39bbdca2
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-03-16 11:48:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,9 @@
  ** Return: always 1.
  **/
 int main(void)
-	i
+	
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }	
 	
