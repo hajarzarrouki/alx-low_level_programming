126f4fae67e09122313e0f4b30c314cb092037f8
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-03-16 11:44:09 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- ** main - A c pogram that prints a line to the standard error
- ** Return: always 1.
- **/
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error 
+ * Return: always 1.
+ */
 int main(void)
+	i
 {
         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
