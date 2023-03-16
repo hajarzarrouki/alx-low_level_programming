e50ab5330b1b1bb5e21a53a26e1112aee8322f54
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-03-16 10:43:06 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  ** main - Entry point
  ** Return: always 0
@@ -12,4 +11,4 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
-}	
+}
