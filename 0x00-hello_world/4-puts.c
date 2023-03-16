fc657e2e07811d38e8ce535dbb395d4c241507a2
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-03-16 09:29:33 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
@@ -8,6 +6,7 @@
 int main(void)
 {
 	    puts("\"Programming is like building a multilingual puzzle");
-	    return(0)
-		    
-}  
+             return (0);	    
+}
+
+/**
