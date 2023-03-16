d0d4c654a6ce8a9f7c24cb3e4948eaba483ac8b7
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-03-16 11:54:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- ** main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- ** followed by a newline, to standard error 
+ ** main - A c ptogram that prints a line to the standard error 
  ** Return: always 1.
  **/
 int main(void)
 {
-        fputs(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+       	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }	
 	
