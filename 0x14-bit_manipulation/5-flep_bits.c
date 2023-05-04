a68edf85f72f6720ae3307a1c33689432d9a7b35
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-05-04 15:43:02 -0700

    5-flep_bits.c

--- 0x14-bit_manipulation/5-flep_bits.c
+++ 0x14-bit_manipulation/5-flep_bits.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits needed to be.
- *             flipeed to get from one number to another.
- * @n: the number.
- * @m: the number to flip n to.
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
  *
- * Return: the necessary number of bits to flip to get from n to m.
+ * Return: The necessary number of bits to flip to get from n to m.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
@@ -17,6 +17,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		bits += (xor & 1);
 		xor >>= 1;
 	}
+
 	return (bits);
 }
-
