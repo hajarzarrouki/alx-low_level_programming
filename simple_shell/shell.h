e798c754156ce711181074b2e0dde7e7444d7455
hajarzarrouki <hajarzarrouki3@gmail.com>
2023-05-22 16:03:22 -0700

    arg.c

---
+++ simple_shell/shell.h
@@ -0,0 +1,14 @@
+#ifndef _SHELL_
+#define _SHELL_
+
+#include <stdio.h>
+#include <unisted.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(void)
+int main(int ac, char **av)
+int main(int ac, char **av, char **env)
+
+#endif
